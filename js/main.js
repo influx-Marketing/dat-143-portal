c3fc997fca4b60b51b63cc01913bc48bd02cc75e
tomflux666 <tom.cocks@influxmarketing.io>
2018-01-24 09:12:01 +0000

    fade-cover

--- js/main.js
+++ js/main.js
@@ -3,22 +3,18 @@
 
 }).call(this);
 
-$(document).ready(function() {
-  $('div.hidden')
-    .fadeIn(1000)
-    .removeClass('hidden');
-});
-
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    var os = $('.dark-bg').offset().top; // determines how far the top of the div is from the top of the window
-    var ht = $('.dark-bg').height(); //determines the height of the div
-    if(scroll > os + ht){ // adds them together, if you scroll further than the top of the div + its height then carry out if statement
-        $('.fixed').addClass('unfixed');
-    }
-
-});
-
-// else {
-//     	$('.fixed').removeClass('unfixed');
-//     }
+// $(document).ready(function() {
+//   $('div.text')
+//     .fadeIn(1000)
+//     .removeClass('hidden');
+//   $('div.text')
+//     .delay(500)
+//     .fadeOut(1000)
+//     .addClass('hidden');
+//
+//   $('div.content')
+//   .delay(2500)
+//     .fadeIn(1000)
+//     .slideup(1000)
+//     .removeClass('hidden-2');
+// });
